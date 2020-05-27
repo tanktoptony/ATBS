9044038550871e006e77c079f17e68c59143a84a
Anthony Jarina <anthony.jarina@gmail.com>
2020-05-27 14:44:25 -0500

    dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,20 @@
+
+
+myCat = {'size': 'fat',
+         'color': 'gray',
+         'disposition': 'loud'
+         }
+
+print(myCat['size'])
+print('size' in myCat)
+
+print('My cat has ' + myCat['color'] + ' fur.')
+
+print(list(myCat.keys()))
+print(list(myCat.values()))
+print(list(myCat.items()))
+
+print('fat' in myCat.values())
+
+
+
--- ListsVStrings.py
+++ ListsVStrings.py
@@ -12,3 +12,13 @@ cheese = spam
 cheese[1] = 'Hello'
 print(cheese)
 print(spam)
+
+def eggs(someParameter):
+    someParameter.append('Hello')
+
+spam = [1, 2, 3]
+eggs(spam)
+print(spam)
+
+
+
