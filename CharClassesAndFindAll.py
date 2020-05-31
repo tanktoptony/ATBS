6486b460fa6f3b70f5a10b108bdb3ecc11d0f24f
Anthony Jarina <anthony.jarina@gmail.com>
2020-05-31 14:28:20 -0500

    regex day 2

--- CharClassesAndFindAll.py
+++ CharClassesAndFindAll.py
@@ -1,6 +1,77 @@
 import re
 
 
-phoneRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+phoneRegex = re.compile(r'(\d\d\d)-\d\d\d-\d\d\d\d')
 
 # findAll() doesn't give a match object but rather a list
+# refer to table 7-1 in book for shorthand codes for common
+# character classes
+digitRegex = re.compile(r'\d')
+
+# 12 days of christmas exercises
+
+lyrics = '''On the first day of Christmas
+my true love sent to me:
+A Partridge in a Pear Tree
+
+On the second day of Christmas
+my true love sent to me:
+2 Turtle Doves
+and a Partridge in a Pear Tree
+
+
+On the third day of Christmas
+my true love sent to me:
+3 French Hens
+2 Turtle Doves
+and a Partridge in a Pear Tree
+
+On the fourth day of Christmas
+my true love sent to me:
+4 Calling Birds
+3 French Hens
+2 Turtle Doves
+and a Partridge in a Pear Tree
+
+On the fifth day of Christmas
+my true love sent to me:
+5 Golden Rings
+4 Calling Birds
+3 French Hens
+2 Turtle Doves
+and a Partridge in a Pear Tree
+
+On the sixth day of Christmas
+my true love sent to me:
+6 Geese a Laying
+5 Golden Rings
+4 Calling Birds
+3 French Hens
+2 Turtle Doves
+and a Partridge in a Pear Tree
+
+
+On the seventh day of Christmas
+my true love sent to me:
+7 Swans a Swimming
+6 Geese a Laying
+5 Golden Rings
+4 Calling Birds
+3 French Hens
+2 Turtle Doves
+and a Partridge in a Pear Tree'''
+
+xmasRegex = re.compile(r'\d+ \w+')
+print(xmasRegex.findall(lyrics))
+
+xmasRegex = re.compile(r'\d+\s\w+')
+print(xmasRegex.findall(lyrics))
+
+vowelRegex = re.compile(r'[aeiouAEIOU]')
+print(vowelRegex.findall('Robocop eats baby food.'))
+
+doubleVowelRegex = re.compile(r'[aeiouAEIOU]{2}')
+print(doubleVowelRegex.findall('Robocop eats baby food.'))
+
+nonvowelsRegex = re.compile(r'[^aeiouAEIOU]')
+print(nonvowelsRegex.findall('Robocop eats baby food.'))
