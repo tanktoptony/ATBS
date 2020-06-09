f3b73b70e8d064d2bff685746cd77e003ba1034a
Anthony Jarina <anthony.jarina@gmail.com>
2020-06-08 20:10:55 -0500

    Phone and Email Scraper Program

---
+++ phoneAndEmail.py
@@ -0,0 +1,42 @@
+#! python3
+
+import re, pyperclip
+
+# Create a regex object for phone numbers
+phoneRegex = re.compile(r'''
+(
+((\d\d\d)|(\(\d\d\d\)))?        # area code (optional)
+(\s|-)        # first separator
+\d\d\d        # first 3 digits
+-        # separator
+\d\d\d\d        # last 4 digits
+(((ext(\.)?\s)|x)   # extension word-part (optional)
+ (\d{2,5}))?        # extension number part (optional)
+)
+''', re.VERBOSE)
+
+
+# TODO: Create a regex for email addresses
+emailRegex = re.compile(r'''
+# some.+_thing@(\d{2,5}))?.com
+
+[a-zA-Z0-9_.+]+        # name part
+@        # @ symbol
+[a-zA-Z0-9_.+]+         # domain name part
+
+''', re.VERBOSE)
+
+# TODO: Get the text off the clipboard
+text = pyperclip.paste()
+
+# TODO: Extract the email/phone from this text
+extractedPhone = phoneRegex.findall(text)
+extractedEmail = emailRegex.findall(text)
+
+allPhoneNumbers = []
+for phoneNumber in extractedPhone:
+    allPhoneNumbers.append(phoneNumber[0])
+
+# TODO: Copy the extracted email/phone to the clipboard
+results = '\n'.join(allPhoneNumbers) + '\n' + '\n'.join(extractedEmail)
+pyperclip.copy(results)
